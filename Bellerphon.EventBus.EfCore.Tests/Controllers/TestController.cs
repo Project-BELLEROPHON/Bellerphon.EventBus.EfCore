0cc49078361bd9d96c41fa11e3255cd6d8cdbeeb
SandroKavlashvili <kavlashvilisandro1@gmail.com>
2024-11-06 00:02:25 +0400

    context save changes example added for little time

--- Bellerphon.EventBus.EfCore.Tests/Controllers/TestController.cs
+++ Bellerphon.EventBus.EfCore.Tests/Controllers/TestController.cs
@@ -1,5 +1,6 @@
 using Bellerphon.EventBus.EfCore.Abstractions;
 using Bellerphon.EventBus.EfCore.Tests.Contexts;
+using Bellerphon.EventBus.EfCore.Tests.Entities;
 using Bellerphon.EventBus.EfCore.Tests.Events;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,15 +11,22 @@ namespace Bellerphon.EventBus.EfCore.Tests.Controllers;
 public class TestController : ControllerBase
 {
     private readonly IPublisher<TestDbContext> _publisher;
+    private readonly TestDbContext _context;
 
-    public TestController(IPublisher<TestDbContext> publisher)
+    public TestController(IPublisher<TestDbContext> publisher, TestDbContext context)
     {
         _publisher = publisher;
+        _context = context;
     }
     
     [HttpPost]
     public async Task<IActionResult> PostAsync(CancellationToken token)
     {
+        _context.Users.Add(new User()
+        {
+            Name = "Sandro",
+            Email = "sandrika@gmail.com"
+        });
         await _publisher.Publish(
             body: new UserCreatedEvent(){UserName = "Test"},
             headers: null, 
@@ -27,7 +35,7 @@ public class TestController : ControllerBase
             body: new UserCreatedEvent(){UserName = "Test2"},
             headers: null, 
             token);
-        await _publisher.SaveChangesAsync(token);
+        await _context.SaveChangesAsync(token);
         return Ok();
     }
 }
